5a296b7601d12c87ef57786f4534142861d295ad
karthik89gdr <52343734+karthik89gdr@users.noreply.github.com>
2022-01-26 09:27:52 +0000

    Add files via upload

---
+++ 6_practice.py
@@ -0,0 +1,41 @@
+
+# i = 0
+# while i<=10:
+#     print(i)
+#     i = i+1
+# print('reached max number')
+
+
+# fruits = ["apple","mango","lichi","banana"]
+# i = 0
+# while i<len(fruits):
+#     print(fruits[i])
+#     i = i+1
+
+
+# fruits = ["apple","mango","lichi","banana"]
+# for item in fruits:
+#     print(item)
+
+
+# for i in range(1,8):
+#     print(i)
+
+# for i in range(1,8,2):
+#     print(i)
+
+# for i in range(10):
+#     if i>0:
+#         continue
+#     print(i)
+
+# for i in range(10):
+#     if i<=0:
+#         pass
+#     print(i)
+
+for i in range(10):
+    if i>=5:
+        break
+    print(i)
+
