2bcd9e4943cc97e335cab0cb2a8edeae1c158ebd
karthik89gdr <52343734+karthik89gdr@users.noreply.github.com>
2022-01-27 10:26:25 +0000

    Add files via upload

---
+++ 7_function.py
@@ -0,0 +1,13 @@
+# def great(name):
+#     print("hello good morning" + name)
+#
+# great("kaka")
+
+def max(num1,num2,num3)
+    if (num1>num2):
+        if(num1>num3):
+            return num1
+        else:
+            return num2
+    else:
+        
